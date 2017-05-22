bd83d8fe35eecefe0027917e63511fa436c9f50c
Daniel <wenzdaniel@hotmail.de>
2017-05-22 16:43:08 +0200

    table_implement
    fetching order_information from database and storing it depending on the status value [0,1,2].
    displaying in table

---
+++ js/auftragsverwaltung.js
@@ -0,0 +1,59 @@
+/**
+ * @author Daniel
+ */
+
+
+if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+    }
+    
+    else {    
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	 xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        	console.log("in der Callback-Funktion");
+        	var myObj = JSON.parse(this.responseText);
+        	console.log("Darstellung des Objekts in drei Komponenten:" + myObj[0].auftragsnummer[0]);
+            console.log("Darstellung des Objekts in drei Komponenten:" + myObj[1]);
+            console.log("Darstellung des Objekts in drei Komponenten:" + myObj[2]);
+            
+            create_content(myObj);
+       }
+    };
+    xhttp.open("GET", "../php/auftragsuebersicht.php", true);
+    xhttp.send();
+
+
+	function create_content(myObj){
+		
+		var text_in_process = "";
+		var text_in_queue = "";
+		var text_in_done = "";
+		
+		for(var a = 0; a < myObj[1].regal.length; a++){
+			
+		text_in_process +=  "<tr>" +
+                    		"<td>" + myObj[1].contents[a] + "</td>" + 
+                    		"<td>" + myObj[1].regal[a] + "</td>" +  
+                			"</tr>";
+		}
+		
+		text_in_queue =	   "<div class='table-responsive'> <table class='table table-striped'> <thead>"
+                   			+  "<tr> <th>Produkt</th> <th>Regal</th> </tr> </thead>"
+                   			+  "<tbody>";
+        
+        for(var a = 0; a < myObj[0].auftragsnummer.length; a++){
+        	
+       	text_in_queue += 
+        			"<tr>" +
+                    "<td>" + myObj[0].contents[a] + "</td>" + 
+                    "<td>" + myObj[0].regal[a] + "</td>" +  
+                	"</tr>";
+        }
+         	
+        text_in_queue += "</tbody> </table> </div>";
+        
+        $("#in_process").html(text_in_process);
+		$("#in_queue").append(text_in_queue);
+	}
---
+++ php/auftragsuebersicht.php
@@ -0,0 +1,47 @@
+<?php
+
+   include("../includes/mdb_lib.inc.php");
+   
+   global $orders;
+   global $bins;
+   
+   $in_queue = $orders->find(array('status' => 0));
+   $in_progress = $orders->find(array('status' => 1));
+   $in_done = $orders->find(array('status' => 2));
+   
+   $data = array();
+   			 
+   foreach($in_queue as $doc){
+   	   $data[0]['auftragsnummer'][] = $doc['auftragsnummer'];
+	   $data[0]['size_id'][] = $doc['size_id'];
+	   $data[0]['contents'][] = $doc['contents'];
+	   
+	   		foreach($doc['contents'] as $name){
+	   			$res = $bins->findOne(array('contents' => $name));
+				$data[0]['regal'][] = $res['bin_id'];
+	   		}
+	    
+   }
+
+   foreach($in_progress as $doc){
+   	   $data[1]['auftragsnummer'] = $doc['auftragsnummer'];
+	   $data[1]['size_id'] = $doc['size_id'];
+	   $data[1]['contents'] = $doc['contents'];
+	   
+	  		foreach($doc['contents'] as $name){
+	   			$res = $bins->findOne(array('contents' => $name));
+				$data[1]['regal'][] = $res['bin_id'];
+	   		}
+	    
+   }
+   
+   foreach($in_done as $doc){
+   	   $data[2]['auftragsnummer'] = $doc['auftragsnummer'];
+	   $data[2]['size_id'] = $doc['size_id'];
+	   $data[2]['contents'] = $doc['contents'];
+	    
+   }
+   
+   $result = json_encode($data);
+   echo $result;
+?>
